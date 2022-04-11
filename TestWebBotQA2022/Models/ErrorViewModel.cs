bb72bcf6fb8d7d6eaad6df705028d76198adf352
fazilaham <fazil@applab.qa>
2022-04-11 12:11:53 +0300

    testwebbot first commit

---
+++ TestWebBotQA2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestWebBotQA2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
